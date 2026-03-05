0566f381c3d1c2d3cafb90a696c22d25f190f8d7
krys leung <71105576+krys8Git@users.noreply.github.com>
2026-03-05 11:03:17 -0500

    fix: conditional operator update

--- number_guess.sh
+++ number_guess.sh
@@ -2,6 +2,9 @@
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
+# generate secret number
+SECRET=500
+
 # collect username
 echo -e "Enter your username:"
 read USERNAME
@@ -13,25 +16,23 @@ USER_EXIST=$($PSQL "select username from users where username='$USERNAME'")
 if [[ -z $USER_EXIST ]]
 then
   # create user if does not exist
-  $($PSQL "insert into users(username) values('$USERNAME')")
+  CREATE_USER=$($PSQL "insert into users(username, games_played, best_game) values('$USERNAME',0,0)")
   echo -e "Welcome, $USERNAME! It looks like this is your first time here."
-  GAMES_PLAYED=$($PSQL "select games_played from users where username=$USERNAME")
-  BEST_GAME=$($PSQL "select best_game from users where username=$USERNAME")
+  GAMES_PLAYED=$($PSQL "select games_played from users where username='$USERNAME'")
+  BEST_GAME=$($PSQL "select best_game from users where username='$USERNAME'")
 else
   # query info on existing user
-  GAMES_PLAYED=$($PSQL "select games_played from users where username=$USERNAME")
-  BEST_GAME=$($PSQL "select best_game from users where username=$USERNAME")
+  GAMES_PLAYED=$($PSQL "select games_played from users where username='$USERNAME'")
+  BEST_GAME=$($PSQL "select best_game from users where username='$USERNAME'")
   # welcome existing user
   echo -e "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
-# generate secret number
-SECRET=
-
 # guess number
 echo -e "Guess the secret number between 1 and 1000:"
 GUESS_COUNT=0
-while $USER_GUESS != $SECRET
+USER_GUESS=''
+while [[ $USER_GUESS -ne $SECRET ]]
 do
   # collect guess
   read USER_GUESS
@@ -41,14 +42,15 @@ do
   if [[ -z $IS_NUMBER ]]
   then
     # increment guess count
-    GUESS_COUNT=$GUESS_COUNT+1
+    ((GUESS_COUNT++))
 
     # evaluate guess and guide user to correct guess
-    if [[ $USER_GUESS >= $SECRET ]]
+    if [[ $USER_GUESS -gt $SECRET ]]
     then
-
-    else if [[ $USER_GUESS <= $SECRET ]]
-      
+      echo -e "It's lower than that, guess again:"
+    elif [[ $USER_GUESS -lt $SECRET ]]
+    then
+      echo -e "It's higher than that, guess again:"
     fi
   else
     # non integer is guessed
@@ -60,13 +62,13 @@ done
 echo -e "You guessed it in $GUESS_COUNT tries. The secret number was $SECRET. Nice job!"
 
 # check if old best guess is higher than current game
-if [[ $BEST_GUESS > $GUESS_COUNT ]]
+if [[ $BEST_GUESS -gt $GUESS_COUNT ]]
 then
   UPDATE_BEST_GUESS=$($PSQL "update users set best_guess=$BEST_GUESS where username='$USERNAME'")
 fi
 
 # update games played
-GAMES_PLAYED=$GAMES_PLAYED+1
+((GAMES_PLAYED++))
 UPDATE_GAMES_PLAYED=$($PSQL "update users set games_played=$GAMES_PLAYED where username='$USERNAME'")
 
 
